08c5d120398d0cefcbf169387b09b00e0ca2ffcb
zhonglin@bwton.com <Ling199274>
2020-11-02 14:33:15 +0800

    feat(feign):调整配置

--- bookkeeping_service/src/main/java/cn/leo/bookkeeping/user/BookkeepingServiceApplication.java
+++ bookkeeping_service/src/main/java/cn/leo/bookkeeping/user/BookkeepingServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * bookkeeping 启动类
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableFeignClients("cn.leo")
 @MapperScan(basePackages = {"cn.leo.bookkeeping.user"})
 public class BookkeepingServiceApplication {
 
