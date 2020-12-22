a6c0a34a57edc294dc0cf024d31cd03fbe4f3d2d
zhonglin@bwton.com <Ling199274>
2020-12-22 17:38:46 +0800

    feat(docker):add erureka service dockerfile

--- bookkeeping_gateway_service/src/main/java/cn/leo/bookkeeping/BookkeepingGatewayServiceApplication.java
+++ bookkeeping_gateway_service/src/main/java/cn/leo/bookkeeping/BookkeepingGatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package cn.leo.bookkeeping.user;
+package cn.leo.bookkeeping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
